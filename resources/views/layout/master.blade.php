03a0b1decf900c839ce11b98d4b9a68816b24a06
noman <nmn8122@gamil.com>
2026-05-10 23:22:30 +0500

    feat: initialize base layout, dashboard view, and application routing

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,177 @@
+<!doctype html>
+
+<html lang="en" class=" layout-navbar-fixed layout-menu-fixed layout-compact " dir="ltr" data-skin="default"
+    data-bs-theme="light" data-assets-path="../../assets/" data-template="vertical-menu-template-no-customizer">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+    <meta name="robots" content="noindex, nofollow" />
+    <title>Demo: Dashboard - Analytics | Vuexy - Bootstrap Dashboard PRO</title>
+
+    <meta name="description" content="" />
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="../../assets/img/favicon/favicon.ico" />
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap"
+        rel="stylesheet" />
+
+    <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/iconify-icons.css') }}" />
+
+    <script src="{{ asset('assets/vendor/libs/@algolia/autocomplete-js.js') }}"></script>
+
+    <!-- Core CSS -->
+    <!-- build:css assets/vendor/css/theme.css  -->
+
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/node-waves/node-waves.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/core.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}" />
+
+    <!-- Vendors CSS -->
+
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+
+    <!-- endbuild -->
+
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/apex-charts/apex-charts.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/swiper/swiper.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css') }}" />
+    <link rel="stylesheet"
+        href="{{ asset('assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/sweetalert2/sweetalert2.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/flag-icons.css') }}" />
+
+    <!-- Page CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/pages/cards-advance.css') }}" />
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Helpers -->
+    <script src="{{ asset('assets/vendor/js/helpers.js') }}"></script>
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+
+    <script src="{{ asset('assets/js/config.js') }}"></script>
+</head>
+
+<body>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar  ">
+        <div class="layout-container">
+            <!-- Menu -->
+
+            @include('layout.partials.side-bar')
+
+            <div class="menu-mobile-toggler d-xl-none rounded-1">
+                <a href="javascript:void(0);"
+                    class="layout-menu-toggle menu-link text-large text-bg-secondary p-2 rounded-1">
+                    <i class="ti tabler-menu icon-base"></i>
+                    <i class="ti tabler-chevron-right icon-base"></i>
+                </a>
+            </div>
+            <!-- / Menu -->
+
+            <!-- Layout container -->
+            <div class="layout-page">
+
+                @include('layout.partials.nav-bar')
+
+                <!-- Content wrapper -->
+                <div class="content-wrapper">
+
+                    <div class="container-xxl flex-grow-1 container-p-y">
+                        @yield('content')
+                    </div>
+
+
+                </div>
+
+
+                <!-- Content -->
+
+                <!-- / Content -->
+
+                @include('layout.partials.footer')
+                <!-- Content wrapper -->
+                <!-- / Layout page -->
+            </div>
+            <!-- / Layout wrapper -->
+
+            <!-- Core JS -->
+            <!-- build:js assets/vendor/js/theme.js  -->
+
+            <script src="{{ asset('assets/vendor/libs/jquery/jquery.js') }}"></script>
+
+            <script src="{{ asset('assets/vendor/libs/popper/popper.js') }}"></script>
+            <script src="{{ asset('assets/vendor/js/bootstrap.js') }}"></script>
+            <script src="{{ asset('assets/vendor/libs/node-waves/node-waves.js') }}"></script>
+
+
+            <script src="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+
+            <script src="{{ asset('assets/vendor/libs/hammer/hammer.js') }}"></script>
+
+            <script src="{{ asset('assets/vendor/libs/i18n/i18n.js') }}"></script>
+
+            <script src="{{ asset('assets/vendor/js/menu.js') }}"></script>
+
+            <!-- endbuild -->
+
+            <!-- Vendors JS -->
+            <script src="{{ asset('assets/vendor/libs/apex-charts/apexcharts.js') }}"></script>
+            <script src="{{ asset('assets/vendor/libs/swiper/swiper.js') }}"></script>
+            <script src="{{ asset('assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js') }}"></script>
+            <script src="{{ asset('assets/vendor/libs/sweetalert2/sweetalert2.js') }}"></script>
+
+            <!-- Main JS -->
+
+            <script src="{{ asset('assets/js/main.js') }}"></script>
+
+            <!-- Page JS -->
+            <script src="{{ asset('assets/js/dashboards-analytics.js') }}"></script>
+
+
+
+            @yield('scripts')
+            @stack('scripts')
+
+            <!-- Global Form Validation & Modal Management -->
+            <script>
+                $(document).ready(function() {
+                    // 1. Global Bootstrap Validation Logic
+                    var forms = document.querySelectorAll('.needs-validation');
+                    Array.prototype.slice.call(forms).forEach(function(form) {
+                        form.addEventListener('submit', function(event) {
+                            if (!form.checkValidity()) {
+                                event.preventDefault();
+                                event.stopPropagation();
+                            }
+                            form.classList.add('was-validated');
+                        }, false);
+                    });
+
+                    // 2. Global Modal Persistence: Auto-open any modal that has errors inside it
+                    @if ($errors->any())
+                        $('.modal').each(function() {
+                            if ($(this).find('.is-invalid, .invalid-feedback').length > 0) {
+                                var modalId = $(this).attr('id');
+                                if (modalId) {
+                                    var myModal = new bootstrap.Modal(document.getElementById(modalId));
+                                    myModal.show();
+                                }
+                            }
+                        });
+                    @endif
+                });
+            </script>
+</body>
+
+</html>
