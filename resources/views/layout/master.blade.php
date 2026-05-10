8768cef0def2902824a96535fc036763c171c0dc
noman <nmn8122@gamil.com>
2026-05-11 00:06:43 +0500

    commit

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport"
         content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
     <meta name="robots" content="noindex, nofollow" />
-    <title>Demo: Dashboard - Analytics | Vuexy - Bootstrap Dashboard PRO</title>
+    <title>@yield('title', 'Dashboard') | {{ config('app.name', 'Asset Managment') }}</title>
 
     <meta name="description" content="" />
 
